8539752805ab842bfdd8588d0ce996ccbbde3f59
Kevin Hernon <khernon1@gmail.com>
2016-07-12 16:05:28 -0400

    add fans and media controllers and views

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -1,2 +1,35 @@
 class FansController < ApplicationController
+  
+  def new
+    @fan = Fan.new
+  end
+
+  def create
+    @fan = Fan.create(fan_params)
+
+    redirect_to fan_path(@fan)
+  end
+
+  def show
+    @fan = Fan.find(params[:id])
+  end
+
+  def edit
+    @fan = Fan.find(params[:id])
+
+  end
+
+  def update
+   @fan = Fan.find(params[:id])
+   @fan.update(name: params[:fan][:name], bio: params[:fan][:bio])
+   redirect_to fan_path(@fan)    
+  end
+
+
+private
+
+def fan_params
+  params.require(:fan).permit(:name, :bio)
+end
+
 end
--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,2 +1,33 @@
 class MediaController < ApplicationController
+
+  def new
+    @medium = Medium.new
+  end
+
+  def create
+    @medium = Medium.create(medium_params)
+    byebug
+    redirect_to media_path(@medium)
+    #can change the redirect to go to the artist page?
+  end
+
+  def show
+    @medium = Medium.find(params[:id])
+  end
+
+  def edit
+    @medium = Medium.find(params[:id])
+  end
+
+  def update
+    @medium = Medium.find(params[:id])
+    @medium.update(name: params[:medium][:name], genre: params[:medium][:genre], price: params[:medium][:price], quantity: [:medium][:quantity])
+  end
+
+private
+
+def medium_params
+  params.require(:medium).permit(:name, :genre, :price, :quantity)
+end
+
 end
