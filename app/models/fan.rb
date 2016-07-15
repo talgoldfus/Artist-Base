642f3f0e77cb227117c645e8ae6022d4e176e819
Kevin Hernon <khernon1@gmail.com>
2016-07-15 12:51:21 -0400

    add recommended media for fan

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -14,4 +14,22 @@ class Fan < ApplicationRecord
   def artists_in_fans_state
     Artists.where('state = ?', self.state)
   end
+
+  def self.recommended_medium(current_fan)
+    fan_items = current_fan.cart.items.pluck(:medium_id)
+    other_fan_items = []
+      Fan.all.each do |fan|        
+          other_fan_items << fan.cart.items.pluck(:medium_id)
+      end
+      
+      new_arr = []
+      other_fan_items.map do |items|
+          new_arr << (items - fan_items).uniq        
+        end
+    
+    new_arr = (new_arr.sort_by { |a| a.length }).reject(&:empty?)
+    Medium.find new_arr.first[0]
+
+  end
+
 end
