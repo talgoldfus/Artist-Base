3ed577779f44519f4f8df9a72ce89fd7505f5bce
Ryan Palermo <stix250@gmail.com>
2016-07-15 10:03:54 -0400

    add city and state to artist

--- app/models/image_collection.rb
+++ app/models/image_collection.rb
@@ -3,6 +3,7 @@ class ImageCollection < ApplicationRecord
   has_many :media
   accepts_nested_attributes_for :media, reject_if: proc {|attributes| attributes['name'].blank?}
 
+  validates_presence_of :name, :img_link
   validates_format_of :img_link, :with => %r{\Ahttp.+\.(gif|jpe?g|png)}i, :message => "must have an image extension"
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,22 @@
 #Faker::Avatar.image("my-own-slug", "50x50")
 img = 1
 Artist.destroy_all
-10.times do 
-   Artist.create(name: Faker::Name.name, abstract: Faker::ChuckNorris.fact, 
-    bio: Faker::Hacker.say_something_smart, password: "flatiron", 
+10.times do
+   Artist.create(name: Faker::Name.name, abstract: Faker::ChuckNorris.fact,
+    bio: Faker::Hacker.say_something_smart, password: "flatiron",
     username: Faker::Internet.email, artist_type: "Photographer",
-    img_link: Faker::Avatar.image("image#{img}", "200x200"))
+    img_link: Faker::Avatar.image("image#{img}", "200x200"),
+    city: Faker::Address.city, state: Faker::Address.state)
    img += 1
 end
 
 ##fans
 Fan.destroy_all
-20.times do 
-  fan =Fan.create(name: Faker::Name.name, bio: Faker::ChuckNorris.fact, 
+20.times do
+  fan =Fan.create(name: Faker::Name.name, bio: Faker::ChuckNorris.fact,
     password: "flatiron", username: Faker::Internet.email,
     img_link: Faker::Avatar.image("image#{img}", "200x200"))
-    img += 1 
+    img += 1
   fan.cart=Cart.create()
   fan.save
 end
@@ -26,20 +27,20 @@ word_arr = %w[Tampflex Opela Wrapsafe Kanlam Greenlam Lotlux Cardguard Treeflex 
 
 ImageCollection.destroy_all
 n = 0
-20.times do 
+20.times do
   ImageCollection.create(name: word_arr[n], artist_id: rand(1..10),
     img_link: Faker::Avatar.image("image#{img}", "200x200"))
   n += 1
   img += 1
 end
 
-##media 
+##media
 genre_arr = ["Horror", "Humor", "Biography/Autobiography", "Comic/Graphic", "Novel", "Western", "Science", "Fiction"]
 
 Medium.destroy_all
 n = 0
-40.times do 
-  Medium.create(name: Faker::Name.name,  
+40.times do
+  Medium.create(name: Faker::Name.name,
     quantity: rand(1..100), genre: genre_arr[n], image_collection_id: rand(1..20),
     img_link: Faker::Avatar.image("image#{img}", "200x200"))
     img += 1
@@ -52,4 +53,3 @@ Item.destroy_all
 100.times do
   Item.create(cart_id: rand(1..20), medium_id: rand(1..40))
 end
-
