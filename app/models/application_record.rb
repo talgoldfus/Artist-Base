955e8b430430cd5cf90b07e1d0fe678c46848715
Tal Goldfus <talgoldfus@gmail.com>
2016-07-15 10:04:31 -0400

    Add recently added methods to cart

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def self.recently_created(num)
-   
-      self.all.order("id DESC").limit(num)
-
-  
+  def self.recently_created(num) 
+      self.all.order("id DESC").limit(num) 
   end
 
  
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,17 @@ class Cart < ApplicationRecord
     self.media.map {|medium| medium.image_collection}.uniq
   end
 
+  def recently_added_medium
+       Medium.find(self.items.last.medium_id)
+  end
+
+  def recently_added_collection
+       Medium.find(self.items.last.medium_id).image_collection
+  end
+
+  def recently_added_artist
+      Medium.find(self.items.last.medium_id).image_collection.artist
+  end
+
+
 end
