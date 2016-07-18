538318d63e0a78528d0145ea3c9964abb13b03f7
Kevin Hernon <khernon1@gmail.com>
2016-07-17 21:53:11 -0400

    uncommented artist adapter

--- app/models/services/artist_adapter.rb
+++ app/models/services/artist_adapter.rb
@@ -3,11 +3,11 @@ module Services
     def get_artists#(artist_name)
       response = Services::HarvardClient.new.create_artists
         i = Artist.all.count + 1    
-      response.parsed_response["records"]#.each do |item|
-      #   Artist.create(name: item["people"][0]["name"], bio: "testing", abstract: "testing", username: "artist#{i}", 
-      #     password: "flatiron", img_link: item["primaryimageurl"])
-      #   i += 1        
-      # end
+      response.parsed_response["records"].each do |item|
+        Artist.create(name: item["people"][0]["name"], bio: "testing", abstract: "testing", username: "artist#{i}", 
+          password: "flatiron", img_link: item["primaryimageurl"])
+        i += 1        
+      end
     end
   end
 end
